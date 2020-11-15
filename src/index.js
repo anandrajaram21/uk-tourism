60160ae51d9c69c4961841477fc6c14b06e6d520
anandrajaram21 <anandrajaram21@gmail.com>
2020-11-15 17:21:04 +0530

    screw github pages, netlify is cool

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Typed from "typed.js";
+
+var options = {
+  strings: ["Uttarakhand is beautiful", "Uttarakhand is magnificent"],
+  typeSpeed: 40,
+  loop: true,
+};
+
+var typed = new Typed(".testing", options);
