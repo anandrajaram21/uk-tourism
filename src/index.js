eea9072fc32a3b7ed06c60dfb22cac6ff08d936e
anandrajaram21 <anandrajaram21@gmail.com>
2020-11-15 17:31:01 +0530

    testing ci and cd on netlify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Typed from "typed.js";
 
 var options = {
-  strings: ["Uttarakhand is beautiful", "Uttarakhand is magnificent"],
+  strings: ["Uttarakhand is superb", "Uttarakhand is awesome"],
   typeSpeed: 40,
   loop: true,
 };
