4525dfd144bcc8f72e02ca44f147a379374bbc97
anandrajaram21 <anandrajaram21@gmail.com>
2020-11-16 10:40:19 +0530

    added typed.js and started using it. made sample app too

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
 <script>
 	import TailwindCss from './TailwindCss.svelte';
+	import { onMount } from 'svelte';
+	import Typed from 'typed.js';
+	var options = {
+		strings: ['hello', 'goodbye'],
+		typeSpeed: 40,
+		loop: true
+	}
+	onMount(() => {
+		var typed  = new Typed('.typing', options);
+	})
 	export let name;
 </script>
 <TailwindCss/>
 
-<main>
-	<h1 class="bg-blue-500">Hello {name}!</h1>
-</main>
+<body>
+	<h1 class="bg-blue-900 text-white p-5">Hello {name}!</h1>
+	<div class="bg-red-500 p-5 text-white">
+		<p class="typing inline"></p>
+	</div>
+</body>
