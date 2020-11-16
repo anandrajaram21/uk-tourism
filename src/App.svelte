229a4b04b039a64b18ee618e26191c731cb55f77
anandrajaram21 <anandrajaram21@gmail.com>
2020-11-16 09:23:20 +0530

    tailwindcss seems damn cool, and so does svelte

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	import TailwindCss from './TailwindCss.svelte';
+	export let name;
+</script>
+<TailwindCss/>
+
+<main>
+	<h1 class="bg-blue-500">Hello {name}!</h1>
+</main>
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Typed from "typed.js";
-
-var options = {
-  strings: ["Uttarakhand is superb", "Uttarakhand is awesome"],
-  typeSpeed: 40,
-  loop: true,
-};
-
-var typed = new Typed(".testing", options);
